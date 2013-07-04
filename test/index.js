903267844b4171dd822cb2df77a1e010efb2de3b
nulltask <nulltask@gmail.com>
2013-07-04 23:03:52 +0900

    update test

--- test/index.js
+++ test/index.js
@@ -16,30 +16,42 @@ describe('emit-logger', function() {
     var emitter1 = new Emitter();
     var emitter2 = new Emitter();
     
-    it('#add()', function() {
+    it('#add(emitter1)', function() {
+      logger.once('add', function(emitter) {
+        assert(emitter1 === emitter);
+        assert(1 === logger._emitters.length);
+      });
       logger.add(emitter1);
+    });
+
+    it('#add(emitter2)', function() {
+      logger.once('add', function(emitter) {
+        assert(emitter2 === emitter);
+        assert(2 === logger._emitters.length);
+      });
       logger.add(emitter2);
-      assert(2 == logger._emitters.length);
     });
     
-    it('#remove()', function() {
+    it('#remove(emitter2)', function() {
+      logger.once('remove', function(emitter) {
+        assert(emitter2 === emitter);
+        assert(1 == logger._emitters.length);
+        assert(emitter1 === logger._emitters[0]);
+      });
       logger.remove(emitter2);
-      
-      assert(1 == logger._emitters.length);
     });
     
-    it('.Store#add()', function(done) {
+    it('.Store#add()', function() {
       logger._store = {
         add: function(emitter, args) {
           assert(emitter === emitter1);
           assert('foo' === args[0]);
           assert('hello' === args[1]);
           assert('world' === args[2]);
-          done();
         }
       };
-      emitter1.emit('foo', 'hello', 'world');
       emitter2.emit('foo', 'hello', 'world');
+      emitter1.emit('foo', 'hello', 'world');
     });
   });
 });
